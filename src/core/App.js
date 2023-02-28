b4bad67a94dee55ec6b132dc814b00a7e9e54bcd
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-02-28 21:36:31 +0100

    Header added

--- src/core/App.js
+++ src/core/App.js
@@ -1,10 +1,11 @@
 import ContentContainer from "../components/Layout/ContentContainer";
 import Footer from "../components/Layout/Footer";
+import Header from "../components/Layout/Header";
 
 function App() {
-
   return (
     <>
+      <Header />
       <ContentContainer />
       <Footer />
     </>
