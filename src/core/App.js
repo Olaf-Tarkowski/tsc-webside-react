ae60a685fdbbad6f01db22129aef1e353caa0352
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-02-28 14:09:15 +0100

    add Footer and ContentContainer

--- src/core/App.js
+++ src/core/App.js
@@ -1,5 +1,14 @@
+import ContentContainer from "../components/Layout/ContentContainer";
+import Footer from "../components/Layout/Footer";
+
 function App() {
-  return <div>hej</div>;
+
+  return (
+    <>
+      <ContentContainer />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
